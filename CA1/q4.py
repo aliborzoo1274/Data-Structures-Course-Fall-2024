8492ac4456920676b2ee682214ebae1ce23ae338
Ali Borzoozadeh <aliborzoo1274@gmail.com>
2024-10-10 12:32:31 +0330

    Fixed some issues

--- CA1/q4.py
+++ CA1/q4.py
@@ -1,27 +1,3 @@
-# while True:
-#     input = input().split()
-#     if input[0] == 1:
-#         my_list = [None] * 3
-#     if input[0] == 2:
-#         my_list = []
-#     if input[0] == 3:
-#         my_list.append(input[1])
-#     if input[0] == 4:
-#         if len(my_list) == 0:
-#             print('nulle')
-#         elif input[1] > len(my_list):
-#             print('oute')
-#         else:
-#             print(my_list[input[1] + 1])
-#     if input[0] == 5:
-#         if input[1] == 0:
-#             print('sefre')
-#         else:
-#             print(input[2] / input[1])
-#     if input[0] == 6:
-#         break
-
-
 while True:
     user_input = input().split()
     command = int(user_input[0])
